e1cde9ba0699631bd387d705b83b9b52b92e4428
Protheu <everaldo.sobrinho42@gmail.com>
2024-07-03 17:33:29 -0300

    All files configured for been a template

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # webpack-prettier-eslint-template
-Template repository webpack, prettier and eslint already configured 
+
+Template repository webpack, prettier and eslint already configured
+
+- Create dist folder on root
+- Prettier is configured to auto-format every times the documents save. (Inside .vscode folder)
+- If you run "npx webpack --watch" you will not have to rerun webpack every time you make a change
+
+# COMMANDS TO USE
+
+npm init // create a new json if needed
+npm install webpack webpack-cli --save-dev
+npm install --save-dev style-loader css-loader
+npm init @eslint/config@latest
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebPackPlugin ({
+            template: './src/index.html',
+            filename: 'index.html',
+            inject: 'body',
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpeg|jpg|svg).$/i,
+                use: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+              },
+        ],
+    },
+}
